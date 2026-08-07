d9e4a5b0622c8a2570d8a55443a3aecd668dce38
Alexsandro Goes <alexsandrogoes7@gmail.com>
2026-08-07 04:44:42 -0300

    Update

---
+++ Full-Stack/Java Script/Java Script Intermediário/Array/ex.001/scripts.js
@@ -0,0 +1,11 @@
+//Criando um Array com construtor
+const newArray = new Array ()
+console.log(newArray)
+console.log("Rodrigo".length)
+console.log(newArray.length)
+
+//[] - Array
+//{} - Object
+
+//Criar Array com 10 posições vazias
+const available = new Array(10)
